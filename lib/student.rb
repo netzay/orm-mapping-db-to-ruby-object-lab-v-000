f00f19db7e0d556d1037a8a05a382aa99afa100e
Ally Netzel <netzelally@gmail.com>
2018-05-19 15:35:25 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -80,10 +80,11 @@ class Student
     SELECT * 
     FROM students
     WHERE students.grade == 10
+    LIMIT 1
     SQL
     DB[:conn].execute(sql).collect do |row|
       self.new_from_db(row)
-    end
+    end.first
   end
 
   def self.drop_table
