d854b707611fe2837c64822d3d9b9fda78c26dbe
Ally Netzel <netzelally@gmail.com>
2018-05-19 15:41:10 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -75,7 +75,7 @@ class Student
     end
   end
   
-   def self.first_x_students_in_grade_10(num)
+   def self.first_X_students_in_grade_10(num)
     sql = <<-SQL
     "SELECT * 
     FROM students 
