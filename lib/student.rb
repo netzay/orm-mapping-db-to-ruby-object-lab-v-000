fe30a00c321ea551ba9ef4f67bc2d92edf03c31e
Ally Netzel <netzelally@gmail.com>
2018-05-19 15:39:59 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -75,16 +75,11 @@ class Student
     end
   end
   
-  def self.first_X_students_in_grade_10
-    sql = <<-SQL
-    SELECT * 
-    FROM students
-    WHERE students.grade = 10
-    LIMIT 1
-    SQL
-    DB[:conn].execute(sql).collect do |row|
-      self.new_from_db(row)
-    end.first
+   def self.first_x_students_in_grade_10(num)
+    sql = "SELECT * 
+    FROM students WHERE grade=10 ORDER BY students.id LIMIT ?;"
+    DB[:conn].execute(sql, num)
+    end
    end	   
 
   def self.drop_table
