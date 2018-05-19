a01f1b60a718368714f12bf9d9d2c2eb73e6bc43
Ally Netzel <netzelally@gmail.com>
2018-05-19 15:46:46 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -93,7 +93,7 @@ class Student
       LIMIT 1
     SQL
  
-    DB[:conn].execute(sql, name).collect do |row|
+    DB[:conn].execute(sql).collect do |row|
       self.new_from_db(row)
     end.first
   end
