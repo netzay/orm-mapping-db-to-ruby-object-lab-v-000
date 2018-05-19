f75a5e3e019e7af48782b5d8bc58daf0c2facfa8
Ally Netzel <netzelally@gmail.com>
2018-05-19 15:29:58 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -66,7 +66,7 @@ class Student
   
   def self.students_below_12th_grade
      sql = <<-SQL
-    SELECT students.name FROM students WHERE grade <= 11;
+    SELECT name FROM students WHERE grade <= 11;
 
       SQL
     
